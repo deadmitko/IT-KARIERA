eb362f7b09662de08cfd28d0a30f011070c19136
deadmitko <deadmitko@gmail.com>
2018-09-01 22:47:00 +0300

    Add files via upload

---
+++ Game/Game/CapacityList.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game
+{
+    class CapacityList
+    {
+
+        private const int InitialCapacity = 2;
+        private Pair[] items;
+        private int startIndex = 0;
+        private int nextIndex = 0;
+        public CapacityList(int capacity = InitialCapacity)
+        {
+            this.items = new Pair[capacity];
+        }
+
+        public int Count { get; private set; }
+
+        public Pair SumIntervalPairs()
+        {
+            Pair result = new Pair(0, 0);
+            for (int i = startIndex; i < nextIndex; i++)
+            {
+                result.First = result.First + this.items[i].First;
+                result.Last = result.Last + this.items[i].Last;
+            }
+            return result;
+        }
+
+        public Pair Sum()
+        {
+            Pair result = new Pair(0, 0);
+            for (int i = 0; i < this.Count; i++)
+            {
+                result.First = result.First + this.items[i].First;
+                result.Last = result.Last + this.items[i].Last;
+            }
+            return result;
+        }
+
+        public void Add(Pair item)
+        {
+            if (nextIndex == items.Length && Count < items.Length)
+            {
+                items[startIndex] = SumIntervalPairs();
+                startIndex++;
+                Count++;
+                nextIndex = startIndex;
+            }
+            if (nextIndex < items.Length)
+            {
+                items[nextIndex++] = item;
+            }
+        }
+
+        public void PrintCurrentState()
+        {
+            for (int i = 0; i < nextIndex; i++)
+            {
+                Console.WriteLine(items[i].ToString());
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Game/Game/Pair.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game
+{
+    class Pair
+    {
+        public int First { get; set; }
+        public int Last { get; set; }
+
+        public Pair(int first, int last)
+        {
+            this.First = first;
+            this.Last = last;
+        }
+
+        public override string ToString()
+        {
+            return $"({this.First}, {this.Last})";
+        }
+
+        public int Difference()
+        {
+            return Math.Abs(this.First - this.Last);
+        }
+
+
+
+    }
+}
